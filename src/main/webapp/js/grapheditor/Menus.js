8a2ae7c9a335466b04cbb60029e6d6eaf211d9e1
Jon Knight <jon@metrixapps.com>
2021-02-08 15:17:22 -0500

    update

--- src/main/webapp/js/grapheditor/Menus.js
+++ src/main/webapp/js/grapheditor/Menus.js
@@ -30,7 +30,7 @@ Menus.prototype.defaultFontSize = '12';
 /**
  * Sets the default font size.
  */
-Menus.prototype.defaultMenuItems = ['file', 'edit', 'view', 'arrange', 'extras', 'help'];
+Menus.prototype.defaultMenuItems = ['file', 'edit', 'view', 'arrange', 'extras', 'help', "Xano Sync"];
 
 /**
  * Adds the label menu items to the given menu and parent.
